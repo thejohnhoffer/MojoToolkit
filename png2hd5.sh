146f4be2294d984f0e01e5ea8850201aae1d68a8
thejohnhoffer <john@hoff.in>
2016-11-01 14:35:52 -0400

    done

--- png2hd5.sh
+++ png2hd5.sh
@@ -3,3 +3,4 @@ mkdir /data/Red_Cylinder/$1-$2-$3
 python png2hd5.py /data/x/$1/$2/$3/image /data/Red_Cylinder/$1-$2-$3/image.h5 -d 2
 python png2hd5.py /data/x/$1/$2/$3/synapse-gt-segmentation /data/Red_Cylinder/$1-$2-$3/truth.h5 -d 2 -f bgr
 python png2hd5.py /data/x/$1/$2/$3/synapse-segmentation /data/Red_Cylinder/$1-$2-$3/guess.h5 -d 2 -f bgr
+python png2hd5.py /data/x/$1/$2/$3/mask /data/Red_Cylinder/$1-$2-$3/mask.h5 -d 2 -f bgr
